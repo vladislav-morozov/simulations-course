3afa4b494151b183da288c744cbc0ac58e2548db
Vladislav Morozov <86678044+vladislav-morozov@users.noreply.github.com>
2025-11-10 16:31:10 +0100

    Rename orchestrator file

--- example-codes/2-joint-vs-multiple-tests/sim_infrastructure/orchestrators.py
+++ example-codes/2-joint-vs-multiple-tests/sim_infrastructure/orchestrators.py
@@ -1,11 +1,9 @@
 """
 Module for executing many Monte Carlo scenario.
 
-This module contains the SimulationOrchestrator class, which executes a
-supplied collection of SimulationScenario objects.
-
 Classes:
-    SimulationOrchestrator: runs simulation scenarios and stores summaries.
+    SimulationOrchestratorParallel: runs simulations in parallel
+    SimulationOrchestratorSequential: runs simulations sequentially
 """
 
 from concurrent.futures import ThreadPoolExecutor, as_completed
@@ -14,30 +12,6 @@ from tqdm import tqdm
 
 from sim_infrastructure.runner import SimulationRunner
 from sim_infrastructure.scenarios import SimulationScenario
-
-
-class SimulationOrchestratorSequential:
-    """Sequential simulation orchestrator class."""
-
-    def __init__(self, scenarios: list[SimulationScenario]) -> None:
-        self.scenarios = scenarios
-        self.summary_results = []
-
-    def run_single_scenario(self, scenario):
-        """Run a single scenario and return the result dictionary."""
-        dgp = scenario.dgp(**scenario.dgp_params)
-        estimator = scenario.test(**scenario.test_params)
-        runner = SimulationRunner(dgp, estimator)
-        runner.simulate(
-            n_sim=scenario.n_simulations,
-            n_obs=scenario.sample_size,
-            first_seed=scenario.first_seed,
-        )
-        return runner.summarize_results()
-
-    def run_all(self):
-        for scenario in tqdm(self.scenarios, desc="Running simulations"):
-            self.summary_results.append(self.run_single_scenario(scenario))
 
 
 class SimulationOrchestratorParallel:
@@ -72,3 +46,27 @@ class SimulationOrchestratorParallel:
                 as_completed(futures), total=len(futures), desc="Running simulations"
             ):
                 self.summary_results.append(future.result())
+
+
+class SimulationOrchestratorSequential:
+    """Sequential simulation orchestrator class."""
+
+    def __init__(self, scenarios: list[SimulationScenario]) -> None:
+        self.scenarios = scenarios
+        self.summary_results = []
+
+    def run_single_scenario(self, scenario):
+        """Run a single scenario and return the result dictionary."""
+        dgp = scenario.dgp(**scenario.dgp_params)
+        estimator = scenario.test(**scenario.test_params)
+        runner = SimulationRunner(dgp, estimator)
+        runner.simulate(
+            n_sim=scenario.n_simulations,
+            n_obs=scenario.sample_size,
+            first_seed=scenario.first_seed,
+        )
+        return runner.summarize_results()
+
+    def run_all(self):
+        for scenario in tqdm(self.scenarios, desc="Running simulations"):
+            self.summary_results.append(self.run_single_scenario(scenario))
