2d2a86329321e054f90ab0aac82e97fa1921a21a
Vladislav Morozov <86678044+vladislav-morozov@users.noreply.github.com>
2025-11-10 12:55:30 +0100

    Add tests

---
+++ example-codes/2-joint-vs-multiple-tests/tests/joint.py
@@ -0,0 +1,53 @@
+"""
+Module for joint tests for joint hypothesis.
+
+Classes:
+    WaldWithOLS: OLS-based Wald test that all non-intercept coefficients are zero.
+"""
+
+import numpy as np
+import pandas as pd
+from statsmodels.regression.linear_model import OLS
+
+
+class WaldWithOLS:
+    """Class for applying Wald test to test null of zero coefficients.
+
+    Tailored to linear model
+        Y_i = theta0 + theta1 X_{i1} + ... + thetap X_{ip} + U_i
+    Tests the null:
+        H0: theta1 = ... = thetap = 0
+
+    Attributes:
+        name (str): test name, "Wald"
+        decision (np.bool): whether null is rejected
+    """
+
+    def __init__(self) -> None:
+        self.name: str = "Wald"
+        self.decision: np.bool
+
+    def test(self, x: pd.DataFrame, y: pd.DataFrame) -> None:
+        """Carry out the Wald test with given data.
+
+        Args:
+            x (pd.DataFrame): covariates, including a leading constant column.
+            y (pd.DataFrame): outcomes.
+        """
+        # Fit models
+        lin_reg = OLS(y, x)
+        lin_reg_fit = lin_reg.fit()
+
+        # Perform Wald test
+        num_covars = x.shape[1]
+        wald_restriction_matrix = np.concatenate(
+            (np.zeros((num_covars - 1, 1)), np.eye(num_covars - 1)), axis=1
+        )
+
+        wald_test = lin_reg_fit.wald_test(
+            wald_restriction_matrix,
+            use_f=False,
+            scalar=True,
+        )
+
+        self.decision = wald_test.pvalue <= 0.05
---
+++ example-codes/2-joint-vs-multiple-tests/tests/multiple.py
@@ -0,0 +1,49 @@
+"""
+Module for multiple tests for joint hypothesis.
+
+Classes:
+    BonferronigMultipleTWithOLS: OLS-based multiple t-test that all non-intercept
+        coefficients are zero. Uses Bonferroni correction.
+"""
+
+import numpy as np
+import pandas as pd
+from statsmodels.regression.linear_model import OLS
+from statsmodels.stats.multitest import multipletests
+
+
+class BonferronigMultipleTWithOLS:
+    """Class for applying multiple t-test to test null of zero coefficients.
+
+    Tailored to linear model
+        Y_i = theta0 + theta1 X_{i1} + ... + thetap X_{ip} + U_i
+    Tests the null:
+        H0: theta1 = ... = thetap = 0
+
+    Attributes:
+        name (str): test name, "Bonferroni t"
+        decision (np.bool): whether null is rejected
+    """
+
+    def __init__(self) -> None:
+        self.name: str = "Bonferroni t"
+        self.decision: np.bool
+
+    def test(self, x: pd.DataFrame, y: pd.DataFrame) -> None:
+        """Carry out the multiple t-test with given data.
+
+        Args:
+            x (pd.DataFrame): covariates, including a leading constant column.
+            y (pd.DataFrame): outcomes.
+        """
+        # Fit models
+        lin_reg = OLS(y, x)
+        lin_reg_fit = lin_reg.fit()
+
+        # Perform multiple t-tests
+        p_vals_t = lin_reg_fit.pvalues.iloc[1:]
+        t_test_corrected_bonf = multipletests(
+            p_vals_t,
+            method="bonferroni",  # Bonferroni
+        )
+        self.decision = t_test_corrected_bonf[0].sum() > 0
