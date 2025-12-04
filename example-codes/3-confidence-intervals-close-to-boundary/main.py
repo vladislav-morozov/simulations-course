a9aa54f7f85646e66af79330ad45187cb7d01fe2
Vladislav Morozov <86678044+vladislav-morozov@users.noreply.github.com>
2025-12-04 13:56:25 +0100

    Fix typos in docstring of main.py

--- example-codes/3-confidence-intervals-close-to-boundary/main.py
+++ example-codes/3-confidence-intervals-close-to-boundary/main.py
@@ -1,8 +1,8 @@
 """
 Entry point for running simulations and generating plots.
 
-Simulation 3: evaluating confidence intervals for variance of the normal
-distribution based on the maximum likelihood estimator. Simultions consider
+Simulation 3: evaluating confidence intervals for the variance of the normal
+distribution based on the maximum likelihood estimator. Simulations consider
 coverage and (scaled) length properties as true variance decreases to zero.
 
 This script:
@@ -18,7 +18,7 @@ import numpy as np
 from sim_infrastructure.core import run_simulations
 from sim_infrastructure.plotting import plot_coverage_and_lengths, plot_error_kdes
 
-# Simulation parameters: note upper case for constants
+# Simulation parameters: note using upper case for constants
 TRUE_VARS = np.logspace(-3, 1, 50)
 N_OBS_LIST = [20, 100]
 N_SIMS = 50000
